35dbb4963db21bbf7dae5590d950406669007862
czs <214492169@qq.com>
2020-08-03 15:50:42 +0800

    1111

--- src/main/java/com/springtest/MyHandler.java
+++ src/main/java/com/springtest/MyHandler.java
@@ -3,14 +3,17 @@ package com.springtest;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class MyHandler implements Controller {
-    public ModelAndView handleRequest(javax.servlet.http.HttpServletRequest httpServletRequest, javax.servlet.http.HttpServletResponse httpServletResponse) throws Exception {
-        //装载模型数据和逻辑视图
-        ModelAndView modelAndView = new ModelAndView();
-        //添加模型数据
-        modelAndView.addObject("name","Tom");
-        //添加逻辑视图
-        modelAndView.setViewName("show");
-        return modelAndView;
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+            //装载模型数据和逻辑视图
+            ModelAndView modelAndView = new ModelAndView();
+            //添加模型数据
+            modelAndView.addObject("name","Tom");
+            //添加逻辑视图
+            modelAndView.setViewName("show");
+            return modelAndView;
     }
 }
